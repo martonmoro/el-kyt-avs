b977d869afd54b6c301245251ec97a19dac82a30
Marton Moro <marton.moro@dakai.io>
2024-03-01 11:49:12 +0100

    chore: change port of local eth 8545 -> 8546

--- Makefile
+++ Makefile
@@ -20,10 +20,10 @@ ___CONTRACTS___: ##
 deploy-eigenlayer-contracts-to-anvil-and-save-state: ## Deploy eigenlayer
 	./tests/anvil/deploy-eigenlayer-save-anvil-state.sh
 
-deploy-incredible-squaring-contracts-to-anvil-and-save-state: ## Deploy avs
+deploy-kyt-contracts-to-anvil-and-save-state: ## Deploy avs
 	./tests/anvil/deploy-avs-save-anvil-state.sh
 
-deploy-all-to-anvil-and-save-state: deploy-eigenlayer-contracts-to-anvil-and-save-state deploy-incredible-squaring-contracts-to-anvil-and-save-state ## deploy eigenlayer, shared avs contracts, and inc-sq contracts 
+deploy-all-to-anvil-and-save-state: deploy-eigenlayer-contracts-to-anvil-and-save-state deploy-kyt-contracts-to-anvil-and-save-state ## deploy eigenlayer, shared avs contracts, and inc-sq contracts 
 
 start-anvil-chain-with-el-and-avs-deployed: ## starts anvil from a saved state file (with el and avs contracts deployed)
 	./tests/anvil/start-anvil-chain-with-el-and-avs-deployed.sh
--- tests/anvil/deploy-avs-save-anvil-state.sh
+++ tests/anvil/deploy-avs-save-anvil-state.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL=http://localhost:8545
+RPC_URL=http://localhost:8546
 PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 # cd to the directory of this script so that this can be run from anywhere
@@ -8,7 +8,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 # start an anvil instance in the background that has eigenlayer contracts deployed
-anvil --load-state eigenlayer-deployed-anvil-state.json --dump-state avs-and-eigenlayer-deployed-anvil-state.json &
+anvil --port 8546 --load-state eigenlayer-deployed-anvil-state.json --dump-state avs-and-eigenlayer-deployed-anvil-state.json &
 cd ../../contracts
 forge script script/KYTDeployer.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -v
 # forge script script/IncredibleSquaringDeployer.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -v
@@ -19,10 +19,10 @@ forge script script/KYTDeployer.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_
 # will keep here in case this PR ever gets merged.
 GENESIS_FILE=$parent_path/genesis.json
 TMP_GENESIS_FILE=$parent_path/genesis.json.tmp
-jq '.number = "'$(cast block-number)'"' $GENESIS_FILE > $TMP_GENESIS_FILE
+jq '.number = "'$(cast block-number --rpc-url $RPC_URL)'"' $GENESIS_FILE > $TMP_GENESIS_FILE
 mv $TMP_GENESIS_FILE $GENESIS_FILE
 
 # we also do this here to make sure the operator has funds to register with the eigenlayer contracts
-cast send 0x860B6912C2d0337ef05bbC89b0C2CB6CbAEAB4A5 --value 10ether --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
+cast send --rpc-url $RPC_URL 0x860B6912C2d0337ef05bbC89b0C2CB6CbAEAB4A5 --value 10ether --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
 # kill anvil to save its state
 pkill anvil
