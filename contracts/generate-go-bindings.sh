c45cf9f71d1511ba94508c5952b3dce01ffa3847
Marton Moro <marton.moro@dakai.io>
2024-03-04 14:36:41 +0100

    chore: modify bindings script

--- contracts/generate-go-bindings.sh
+++ contracts/generate-go-bindings.sh
@@ -23,7 +23,7 @@ rm -rf bindings/*
 forge clean
 forge build
 
-avs_service_contracts="IncredibleSquaringServiceManager IncredibleSquaringTaskManager KYTServiceManager KYTTaskManager"
+avs_service_contracts="KYTServiceManager KYTTaskManager"
 for contract in $avs_service_contracts; do
     create_binding . $contract ./bindings
 done
